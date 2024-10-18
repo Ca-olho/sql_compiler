3b42df764ae33c725a914c7af1a8b16c70e8963a
Raul Bonfante Magri <79473720+Ca-olho@users.noreply.github.com>
2024-10-18 15:43:18 -0300

    tabela SLR
    - add a tabela SLR no sintatico.py
    - correção? de bug ao manipular o _lex.txt

--- python/main.py
+++ python/main.py
@@ -1,8 +1,9 @@
 import lexico
+import sintatico
 
 #Arquivos de entrada e saida (já devem estar criados)
 _in = open("_in.txt","r")
-_lex = open("_lex.txt","r+")
+_lex = open("_lex.txt","w")
 _out = open("_out.txt","w")
 
 #Lista de Tokens descobertos e posição
@@ -12,7 +13,8 @@ token_list = []
 try:
     #Começa analise lexica
     lexico.start(_in,_lex,token_list)
-    
+    _lex = open("_lex.txt","r")
+    sintatico.start(_lex,_out)
     #print(token_list)
 except Exception as e: 
     print("erro", str(e))
